5554581b2dfe588a1963691555a7b428e9180afd
jmoics <jmoics@83cf9114-19b1-7438-263b-0e868cda902f>
2010-11-15 14:06:38 +0000

    - add comments.

--- JX_Market/trunk/src/pe/com/jx_market/controller/PO_EAConsultaProducto.java
+++ JX_Market/trunk/src/pe/com/jx_market/controller/PO_EAConsultaProducto.java
@@ -1,5 +1,8 @@
-/**
- * 
+/*
+ * Author:          jx_market
+ * Revision:        $Rev: 1 $
+ * Last Changed:    $Date: 2010-11-11 10:53:13 -0500 (jue, 11 nov 2010) $
+ * Last Changed By: $Author: Jorge Cueva $
  */
 package pe.com.jx_market.controller;
 
@@ -11,10 +14,10 @@ import org.zkoss.zul.Window;
 public class PO_EAConsultaProducto extends Window
 {
     public void onCreate () {
-        
+
     }
-    
-    public void incluir(String txt) {
+
+    public void incluir(final String txt) {
         //getDesktop().getSession().setAttribute("paginaActual", txt);
         getDesktop().getSession().setAttribute("actualizar", "actualizar");
         Utility.saltar(this, txt);
