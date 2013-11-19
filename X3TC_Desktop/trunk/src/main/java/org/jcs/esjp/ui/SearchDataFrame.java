a30e3fa67b8fed6a2203b687e20cef4253931b3e
jmoics@gmail.com <jmoics@gmail.com@83cf9114-19b1-7438-263b-0e868cda902f>
2013-11-19 22:59:12 +0000

    - update search frame adding event for text fields.

--- X3TC_Desktop/trunk/src/main/java/org/jcs/esjp/ui/SearchDataFrame.java
+++ X3TC_Desktop/trunk/src/main/java/org/jcs/esjp/ui/SearchDataFrame.java
@@ -8,9 +8,12 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -21,10 +24,13 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import org.jcs.esjp.model.ObjectAbstract;
 import org.jcs.esjp.model.ObjectPurchase;
@@ -36,12 +42,17 @@ import org.jcs.esjp.model.StructureNormal;
 import org.jcs.esjp.util.Settings;
 
 public class SearchDataFrame
-    extends JFrame implements ItemListener
+    extends JFrame implements ItemListener, DocumentListener
 {
     MapPanel mappan;
     JComboBox<ObjectPosition> galaxyCombo;
     JComboBox<ObjectPosition> objectsCombo;
+    JFormattedTextField txtQuantity;
+    JFormattedTextField txtNotMinorPer;
+    JFormattedTextField txtMinPrice;
+    JFormattedTextField txtMaxPrice;
     Font font;
+    final Map<String, Integer> mapFilter;
 
     public SearchDataFrame(final MapPanel _mappan)
     {
@@ -56,6 +67,8 @@ public class SearchDataFrame
         structPane.add(getOtherPanel());
 
         container.add(structPane);
+
+        mapFilter = new HashMap<String, Integer>();
         container.add(getObjectsPanel());
     }
 
@@ -130,34 +143,6 @@ public class SearchDataFrame
         }
     }
 
-    @Override
-    public void itemStateChanged(final ItemEvent e)
-    {
-        final Component[] components = ((JCheckBox) e.getSource()).getParent().getComponents();
-        final String actionComm = ((JCheckBox)e.getSource()).getActionCommand();
-        if (Settings.SearchSettings.FACTORY.getKey().equals(actionComm)
-                        || Settings.SearchSettings.DOCK.getKey().equals(actionComm)
-                        || Settings.SearchSettings.SECTOR.getKey().equals(actionComm)) {
-            final Set<String> setFilter = new HashSet<String>();
-            for (final Component component : components) {
-                final JCheckBox checkBox = (JCheckBox) component;
-                if (checkBox.isSelected()) {
-                    setFilter.add(checkBox.getActionCommand());
-                }
-            }
-            buildGalaxyDropDown(setFilter);
-        } else {
-            final Map<String, Integer> mapFilter = new HashMap<String, Integer>();
-            for (final Component component : components) {
-                final JCheckBox checkBox = (JCheckBox) component;
-                if (checkBox.isSelected()) {
-                    //mapFilter.add(checkBox.getActionCommand());
-                }
-            }
-            buildObjectsDropDown(mapFilter);
-        }
-    }
-
     protected Map<String, ObjectPosition> getGalaxyData(final Set<String> _setFilter)
     {
         final Map<String, ObjectPosition> mapPos = new TreeMap<String, ObjectPosition>();
@@ -221,18 +206,24 @@ public class SearchDataFrame
         final JPanel objectsPane = new JPanel();
         objectsPane.setLayout(new BorderLayout());
 
-        buildObjectsDropDown(null);
+        buildObjectsDropDown();
 
         final JPanel filterPanel = new JPanel();
         //filterPanel.setBounds(0, 0, 400, 200);
         filterPanel.setLayout(new GridLayout(4, 2));
 
+        final NumberFormat format = NumberFormat.getIntegerInstance();
+
         final JCheckBox minQuantity = new JCheckBox("MIN Cantidad");
         minQuantity.setFont(font);
         minQuantity.setActionCommand(Settings.SearchSettings.MINQUANTITY.getKey());
         minQuantity.addItemListener(this);
-        final JTextField txtQuantity = new JTextField("1");
+        txtQuantity = new JFormattedTextField(format);
+        txtQuantity.setValue(new Integer(1));
+        txtQuantity.setName(Settings.SearchSettings.MINQUANTITY.getKey());
+        txtQuantity.setEditable(false);
         txtQuantity.setFont(font);
+        txtQuantity.getDocument().addDocumentListener(this);
         filterPanel.add(minQuantity);
         filterPanel.add(txtQuantity);
 
@@ -240,8 +231,12 @@ public class SearchDataFrame
         percentage.setFont(font);
         percentage.setActionCommand(Settings.SearchSettings.NOMINORPERCENT.getKey());
         percentage.addItemListener(this);
-        final JTextField txtNotMinorPer = new JTextField("100");
+        txtNotMinorPer = new JFormattedTextField(format);
+        txtNotMinorPer.setValue(new Integer(100));
+        txtNotMinorPer.setName(Settings.SearchSettings.NOMINORPERCENT.getKey());
+        txtNotMinorPer.setEditable(false);
         txtNotMinorPer.setFont(font);
+        txtNotMinorPer.getDocument().addDocumentListener(this);
         filterPanel.add(percentage);
         filterPanel.add(txtNotMinorPer);
 
@@ -249,8 +244,12 @@ public class SearchDataFrame
         minPrice.setFont(font);
         minPrice.setActionCommand(Settings.SearchSettings.MINPRICE.getKey());
         minPrice.addItemListener(this);
-        final JTextField txtMinPrice = new JTextField("0");
+        txtMinPrice = new JFormattedTextField(format);
+        txtMinPrice.setValue(new Integer(0));
+        txtMinPrice.setName(Settings.SearchSettings.MINPRICE.getKey());
+        txtMinPrice.setEditable(false);
         txtMinPrice.setFont(font);
+        txtMinPrice.getDocument().addDocumentListener(this);
         filterPanel.add(minPrice);
         filterPanel.add(txtMinPrice);
 
@@ -258,8 +257,12 @@ public class SearchDataFrame
         maxPrice.setFont(font);
         maxPrice.setActionCommand(Settings.SearchSettings.MAXPRICE.getKey());
         maxPrice.addItemListener(this);
-        final JTextField txtMaxPrice = new JTextField("10000");
+        txtMaxPrice = new JFormattedTextField(format);
+        txtMaxPrice.setValue(new Integer(100000));
+        txtMaxPrice.setName(Settings.SearchSettings.MAXPRICE.getKey());
+        txtMaxPrice.setEditable(false);
         txtMaxPrice.setFont(font);
+        txtMaxPrice.getDocument().addDocumentListener(this);
         filterPanel.add(maxPrice);
         filterPanel.add(txtMaxPrice);
 
@@ -271,6 +274,7 @@ public class SearchDataFrame
             public void mouseClicked(final MouseEvent e)
             {
                 final ObjectPosition item = (ObjectPosition) objectsCombo.getSelectedItem();
+                analizeObjectsFilter(item);
                 mappan.updateGalaxyMap(item);
             }
 
@@ -285,20 +289,13 @@ public class SearchDataFrame
         return objectsPane;
     }
 
-    protected void buildObjectsDropDown(final Map<String, Integer> _mapFilter) {
-        final Collection<ObjectPosition> lst = getObjectsData(_mapFilter).values();
-        if (objectsCombo != null) {
-            objectsCombo.removeAllItems();
-            for (final ObjectPosition objPos : lst) {
-                objectsCombo.addItem(objPos);
-            }
-        } else {
-            objectsCombo = new JComboBox<ObjectPosition>(lst.toArray(new ObjectPosition[0]));
-            objectsCombo.setFont(font);
-        }
+    protected void buildObjectsDropDown() {
+        final Collection<ObjectPosition> lst = getObjectsData().values();
+        objectsCombo = new JComboBox<ObjectPosition>(lst.toArray(new ObjectPosition[0]));
+        objectsCombo.setFont(font);
     }
 
-    protected Map<String, ObjectPosition> getObjectsData(final Map<String, Integer> _mapFilter)
+    protected Map<String, ObjectPosition> getObjectsData()
     {
         final Map<String, ObjectPosition> mapPos = new TreeMap<String, ObjectPosition>();
         final Map<Integer, Map<Integer, Sector>> matrix = mappan.getMatrix();
@@ -311,40 +308,34 @@ public class SearchDataFrame
                 for (final StructureAbstract struc : sector.getLstStruct()) {
                     if (struc instanceof StructureNormal) {
                         for (final ObjectSale objSale : ((StructureNormal) struc).getObjSale()) {
-                            if (_mapFilter == null || analizeObjectsFilter(objSale, _mapFilter)) {
-                                if (!mapPos.containsKey(objSale.getName())) {
-                                    final ObjectPosition objPos = new ObjectPosition(objSale.getName());
-                                    objPos.getObject2Position().put(struc, position);
-                                    mapPos.put(objSale.getName(), objPos);
-                                } else {
-                                    final ObjectPosition secPos = mapPos.get(objSale.getName());
-                                    secPos.getObject2Position().put(struc, position);
-                                }
+                            if (!mapPos.containsKey(objSale.getName())) {
+                                final ObjectPosition objPos = new ObjectPosition(objSale.getName());
+                                objPos.getObject2Position().put(objSale, position);
+                                mapPos.put(objSale.getName(), objPos);
+                            } else {
+                                final ObjectPosition secPos = mapPos.get(objSale.getName());
+                                secPos.getObject2Position().put(objSale, position);
                             }
                         }
                     } else if (struc instanceof StructureFactory) {
                         for (final ObjectSale objSale : ((StructureFactory) struc).getObjSale()) {
-                            if (_mapFilter == null || analizeObjectsFilter(objSale, _mapFilter)) {
-                                if (!mapPos.containsKey(objSale.getName())) {
-                                    final ObjectPosition objPos = new ObjectPosition(objSale.getName());
-                                    objPos.getObject2Position().put(struc, position);
-                                    mapPos.put(objSale.getName(), objPos);
-                                } else {
-                                    final ObjectPosition secPos = mapPos.get(objSale.getName());
-                                    secPos.getObject2Position().put(struc, position);
-                                }
+                            if (!mapPos.containsKey(objSale.getName())) {
+                                final ObjectPosition objPos = new ObjectPosition(objSale.getName());
+                                objPos.getObject2Position().put(objSale, position);
+                                mapPos.put(objSale.getName(), objPos);
+                            } else {
+                                final ObjectPosition secPos = mapPos.get(objSale.getName());
+                                secPos.getObject2Position().put(objSale, position);
                             }
                         }
                         for (final ObjectPurchase objPur : ((StructureFactory) struc).getObjPurch()) {
-                            if (_mapFilter == null || analizeObjectsFilter(objPur, _mapFilter)) {
-                                if (!mapPos.containsKey(objPur.getName())) {
-                                    final ObjectPosition objPos = new ObjectPosition(objPur.getName());
-                                    objPos.getObject2Position().put(struc, position);
-                                    mapPos.put(objPur.getName(), objPos);
-                                } else {
-                                    final ObjectPosition secPos = mapPos.get(objPur.getName());
-                                    secPos.getObject2Position().put(struc, position);
-                                }
+                            if (!mapPos.containsKey(objPur.getName())) {
+                                final ObjectPosition objPos = new ObjectPosition(objPur.getName());
+                                objPos.getObject2Position().put(objPur, position);
+                                mapPos.put(objPur.getName(), objPos);
+                            } else {
+                                final ObjectPosition secPos = mapPos.get(objPur.getName());
+                                secPos.getObject2Position().put(objPur, position);
                             }
                         }
                     }
@@ -355,32 +346,118 @@ public class SearchDataFrame
         return mapPos;
     }
 
-    protected boolean analizeObjectsFilter(final ObjectAbstract _object,
-                                           final Map<String, Integer> _mapFilter) {
-        boolean valid = true;
-
-        for (final Entry<String, Integer> entry : _mapFilter.entrySet()) {
-            final String key = entry.getKey();
-            final Integer value = entry.getValue();
-            if (key.equals(Settings.SearchSettings.MINQUANTITY.getKey())) {
-                if (_object.getQuantity() < value) {
-                    valid = false;
-                }
-            } else if (key.equals(Settings.SearchSettings.NOMINORPERCENT.getKey())) {
-                /*if (_object.getQuantity() < value) {
-                    valid = false;
-                }*/
-            } else if (key.equals(Settings.SearchSettings.MINPRICE.getKey())) {
-                if (_object.getPrice() < value) {
-                    valid = false;
-                }
-            } else if (key.equals(Settings.SearchSettings.MAXPRICE.getKey())) {
-                if (_object.getPrice() > value) {
-                    valid = false;
+    protected void analizeObjectsFilter(final ObjectPosition _objectPosition) {
+        final List<ObjectAbstract> removes = new ArrayList<ObjectAbstract>();
+        for (final Object obj : _objectPosition.getObject2Position().keySet()) {
+            boolean valid = true;
+            final ObjectAbstract object = (ObjectAbstract) obj;
+            for (final Entry<String, Integer> entry : mapFilter.entrySet()) {
+                final String key = entry.getKey();
+                final Integer value = entry.getValue();
+                if (key.equals(Settings.SearchSettings.MINQUANTITY.getKey())) {
+                    if (object.getQuantity() < value) {
+                        valid = false;
+                    }
+                } else if (key.equals(Settings.SearchSettings.NOMINORPERCENT.getKey())) {
+                    /*if (_object.getQuantity() < value) {
+                        valid = false;
+                    }*/
+                } else if (key.equals(Settings.SearchSettings.MINPRICE.getKey())) {
+                    if (object.getPrice() < value) {
+                        valid = false;
+                    }
+                } else if (key.equals(Settings.SearchSettings.MAXPRICE.getKey())) {
+                    if (object.getPrice() > value) {
+                        valid = false;
+                    }
                 }
             }
+            if (!valid) {
+                removes.add(object);
+            }
         }
-        return valid;
+        for (final ObjectAbstract remove : removes) {
+            _objectPosition.getObject2Position().remove(remove);
+        }
+    }
+
+    @Override
+    public void itemStateChanged(final ItemEvent e)
+    {
+        final Component[] components = ((JCheckBox) e.getSource()).getParent().getComponents();
+        final String actionComm = ((JCheckBox)e.getSource()).getActionCommand();
+        if (Settings.SearchSettings.FACTORY.getKey().equals(actionComm)
+                        || Settings.SearchSettings.DOCK.getKey().equals(actionComm)
+                        || Settings.SearchSettings.SECTOR.getKey().equals(actionComm)) {
+            final Set<String> setFilter = new HashSet<String>();
+            for (final Component component : components) {
+                final JCheckBox checkBox = (JCheckBox) component;
+                if (checkBox.isSelected()) {
+                    setFilter.add(checkBox.getActionCommand());
+                }
+            }
+            buildGalaxyDropDown(setFilter);
+        } else {
+            int cont = 0;
+            for (final Component component : components) {
+                if (component instanceof JCheckBox) {
+                    final JCheckBox checkBox = (JCheckBox) component;
+                    final JTextField txtValue = (JTextField) components[cont + 1];
+                    if (checkBox.isSelected()) {
+                        txtValue.setEditable(true);
+                        mapFilter.put(checkBox.getActionCommand(), Integer.parseInt(txtValue.getText()));
+                    } else {
+                        txtValue.setEditable(false);
+                        mapFilter.remove(checkBox.getActionCommand());
+                    }
+                }
+                cont++;
+            }
+            //buildObjectsDropDown(mapFilter);
+        }
+    }
+
+    @Override
+    public void insertUpdate(final DocumentEvent e)
+    {
+        for (final String str : mapFilter.keySet()) {
+            if (txtQuantity.isEditable() && txtQuantity.getName().equals(str)) {
+                mapFilter.put(str, txtQuantity.getValue() instanceof Integer ? (Integer) txtQuantity.getValue() : 1);
+            }
+            if (txtNotMinorPer.isEditable() && txtNotMinorPer.getName().equals(str)) {
+                mapFilter.put(str, txtNotMinorPer.getValue() instanceof Integer ? (Integer) txtNotMinorPer.getValue() : 100);
+            }
+            if (txtMinPrice.isEditable() && txtMinPrice.getName().equals(str)) {
+                mapFilter.put(str, txtMinPrice.getValue() instanceof Integer ? (Integer) txtMinPrice.getValue() : 0);
+            }
+            if (txtMaxPrice.isEditable() && txtMaxPrice.getName().equals(str)) {
+                mapFilter.put(str, txtMaxPrice.getValue() instanceof Integer ? (Integer) txtMaxPrice.getValue() : 100000);
+            }
+        }
+    }
+
+    @Override
+    public void removeUpdate(final DocumentEvent e)
+    {
+        for (final String str : mapFilter.keySet()) {
+            if (txtQuantity.isEditable() && txtQuantity.getName().equals(str)) {
+                mapFilter.put(str, txtQuantity.getValue() instanceof Integer ? (Integer) txtQuantity.getValue() : 1);
+            }
+            if (txtNotMinorPer.isEditable() && txtNotMinorPer.getName().equals(str)) {
+                mapFilter.put(str, txtNotMinorPer.getValue() instanceof Integer ? (Integer) txtNotMinorPer.getValue() : 100);
+            }
+            if (txtMinPrice.isEditable() && txtMinPrice.getName().equals(str)) {
+                mapFilter.put(str, txtMinPrice.getValue() instanceof Integer ? (Integer) txtMinPrice.getValue() : 0);
+            }
+            if (txtMaxPrice.isEditable() && txtMaxPrice.getName().equals(str)) {
+                mapFilter.put(str, txtMaxPrice.getValue() instanceof Integer ? (Integer) txtMaxPrice.getValue() : 100000);
+            }
+        }
+    }
+
+    @Override
+    public void changedUpdate(final DocumentEvent e)
+    {
     }
 
     public class ObjectPosition
