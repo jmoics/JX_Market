b20fba253209d93ca9a8db9c78e2bd50fde7a320
jmoics <Jorge44128463>
2015-05-08 18:17:04 -0500

    - begin changes for employees.

--- src/main/java/pe/com/jx_market/controller/PO_EAAdministrateEmployeeCreate.java
+++ src/main/java/pe/com/jx_market/controller/PO_EAAdministrateEmployeeCreate.java
@@ -1,25 +1,28 @@
 package pe.com.jx_market.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.zkoss.zk.ui.Desktop;
 import org.zkoss.zk.ui.UiException;
 import org.zkoss.zk.ui.event.Event;
-import org.zkoss.zk.ui.select.annotation.Wire;
-import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Caption;
 import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Comboitem;
+import org.zkoss.zul.Datebox;
 import org.zkoss.zul.Div;
 import org.zkoss.zul.Grid;
+import org.zkoss.zul.Groupbox;
 import org.zkoss.zul.Image;
 import org.zkoss.zul.Label;
 import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Popup;
 import org.zkoss.zul.Row;
 import org.zkoss.zul.Rows;
-import org.zkoss.zul.Window;
+import org.zkoss.zul.Textbox;
 
 import pe.com.jx_market.domain.DTO_Company;
 import pe.com.jx_market.domain.DTO_Employee;
@@ -30,36 +33,189 @@ import pe.com.jx_market.utilities.Constantes;
 import pe.com.jx_market.utilities.ServiceInput;
 import pe.com.jx_market.utilities.ServiceOutput;
 
-public class PO_EAAdministrateEmployee
-    extends SecuredComposer<Window>
+public class PO_EAAdministrateEmployeeCreate
+    extends SecuredWindow
 {
 
-    static Log logger = LogFactory.getLog(PO_EAAdministrateEmployee.class);
-    @Wire
-    private Grid lstEmp;
-    @Wire
+    static Log logger = LogFactory.getLog(PO_EAAdministrateEmployeeCreate.class);
+    private Grid grdEmp;
+    private Textbox txtUser, txtPass, txtNombre, txtApellidos, txtDNI, txtTelefono, txtCelular, txtMail,
+                    txtDireccion, txtCiudad, txtRegion;
+    private Label lblUser, lblNombre, lblApellidos, lblDNI, lblRole, lblTelefono, lblMail, lblCiudad, lblEstado;
+    private Datebox datNac;
+    private Button btnInfo, btnCancel, btnCrear;
+    private Caption capNombre, capInfo;
+    private Groupbox grpEmployees;
     private Popup popDetails;
-    @Wire
-    private Label lblUser, lblName, lblLastName, lblSecLastName, lblDocType, lblDocument, lblRole, lblPhone, lblMail, lblCity, lblStatus;
     private Combobox cmbRole, cmbEstado;
     private DTO_Company company;
-    @WireVariable
-    private BusinessService<DTO_Employee> employeeService;
-    @WireVariable
-    private BusinessService<DTO_Role> roleService;
-    @WireVariable
-    private BusinessService<DTO_User> userService;
-    @WireVariable
-    private Desktop desktop;
+    private BusinessService employeeService, roleService, userService;
 
     @Override
-    public void doAfterCompose(final Window _comp)
-        throws Exception
+    public void realOnCreate()
     {
-        super.doAfterCompose(_comp);
-        company = (DTO_Company) desktop.getSession().getAttribute(Constantes.ATTRIBUTE_COMPANY);
+        grdEmp = (Grid) getFellow("grdEmp");
+        txtUser = (Textbox) getFellow("txtUser");
+        txtPass = (Textbox) getFellow("txtPass");
+        txtNombre = (Textbox) getFellow("txtNombre");
+        txtApellidos = (Textbox) getFellow("txtApellidos");
+        txtDNI = (Textbox) getFellow("txtDNI");
+        txtTelefono = (Textbox) getFellow("txtTelefono");
+        txtCelular = (Textbox) getFellow("txtCelular");
+        txtMail = (Textbox) getFellow("txtMail");
+        txtDireccion = (Textbox) getFellow("txtDireccion");
+        txtCiudad = (Textbox) getFellow("txtCiudad");
+        txtRegion = (Textbox) getFellow("txtRegion");
+        lblUser = (Label) getFellow("lblUser");
+        lblNombre = (Label) getFellow("lblNombre");
+        lblApellidos = (Label) getFellow("lblApellidos");
+        lblRole = (Label) getFellow("lblRole");
+        lblDNI = (Label) getFellow("lblDNI");
+        lblTelefono = (Label) getFellow("lblTelefono");
+        lblMail = (Label) getFellow("lblMail");
+        lblCiudad = (Label) getFellow("lblCiudad");
+        lblEstado = (Label) getFellow("lblEstado");
+        cmbRole = (Combobox) getFellow("cmbRole");
+        cmbEstado = (Combobox) getFellow("cmbEstado");
+        btnInfo = (Button) getFellow("btnInfo");
+        btnCancel = (Button) getFellow("btnCancel");
+        btnCrear = (Button) getFellow("btnCrear");
+        grpEmployees = (Groupbox) getFellow("grpEmployees");
+        capNombre = (Caption) getFellow("capNombre");
+        capInfo = (Caption) getFellow("capInfo");
+        popDetails = (Popup) getFellow("popDetails");
+        employeeService = ContextLoader.getService(this, "employeeService");
+        roleService = ContextLoader.getService(this, "roleService");
+        userService = ContextLoader.getService(this, "userService");
+
+        company = (DTO_Company) getDesktop().getSession().getAttribute("company");
         cargarRoles();
         CargarTabla();
+    }
+
+    public void elimina(final DTO_Employee employee)
+    {
+
+        final ServiceInput input = new ServiceInput(employee);
+        input.setAction("delete");
+        final ServiceOutput output = employeeService.execute(input);
+        if (output.getErrorCode() == Constantes.OK) {
+            logger.info("El contacto se elimino correctamente");
+        } else {
+            logger.error("Error al eliminar contacto");
+
+        }
+        CargarTabla();
+
+    }
+
+    public void editar(final DTO_Employee emp,
+                       final String username,
+                       final String pass,
+                       final String nombre,
+                       final String apellido,
+                       final String dni,
+                       final String telefono,
+                       final String mail,
+                       final Boolean estado,
+                       final Integer role,
+                       final String celular,
+                       final String ciudad,
+                       final String direccion,
+                       final String region)
+    {
+        final DTO_Employee employee = new DTO_Employee(); // nuevo user
+        employee.setEmployeeName(nombre);
+        employee.setEmployeeLastName(apellido);
+        employee.setDocumentNumber(dni);
+        employee.setPhone(telefono);
+        employee.setEmail(mail);
+        employee.setActive(estado);
+        //employee.setRoleId(role);
+        employee.setCompanyId(company.getId());
+        employee.setCellPhone(celular);
+        employee.setCity(ciudad);
+        employee.setAddress(direccion);
+        employee.setUbigeo(region);
+
+        final ServiceInput input = new ServiceInput();
+        final Map<String, Object> map = new HashMap<String,Object>();
+        map.put("employee", employee);
+        if(emp == null) {
+            final DTO_User user = new DTO_User();
+            user.setUsername(username);
+            user.setPassword(pass);
+            user.setCompanyId(company.getId());
+
+            map.put("user", user);
+        } else if (emp != null && pass != null && pass.length() != 0) {
+            //para cambiar pass
+            employee.setId(emp.getId());
+            final DTO_User user = new DTO_User();
+            user.setId(emp.getUserId());
+            user.setUsername(username);
+            user.setPassword(pass);
+            user.setCompanyId(company.getId());
+
+            map.put("user", user);
+        } else {
+            employee.setId(emp.getId());
+        }
+        input.setAction(Constantes.V_REGISTER);
+        input.setMapa(map);
+        final ServiceOutput output = employeeService.execute(input);
+        if (output.getErrorCode() == Constantes.OK) {
+            alertaInfo("El employee se registro correctamente", "El contacto se registro correctamente", null);
+        } else {
+            alertaError("Error al registrar el contacto", "Error al registrar el contacto", null);
+        }
+    }
+
+    public void cancelar()
+    {
+        txtUser.setReadonly(false);
+        CargarTabla();
+    }
+
+    public void actualizar()
+    {
+        if (!txtUser.getValue().equals("") && !txtNombre.getValue().equals("") &&
+                        !txtApellidos.getValue().equals("") && !txtDNI.getValue().equals("") &&
+                        cmbRole.getSelectedItem() != null && cmbEstado.getSelectedItem() != null) {
+
+            editar((DTO_Employee) txtUser.getAttribute("user"), txtUser.getValue(),
+                            txtPass.getValue(), txtNombre.getValue(), txtApellidos.getValue(),
+                            txtDNI.getValue(), txtTelefono.getValue(), txtMail.getValue(),
+                            (Boolean)cmbEstado.getSelectedItem().getValue(),
+                            ((DTO_Role) cmbRole.getSelectedItem().getAttribute("role")).getId(),
+                            txtCelular.getValue(), txtCiudad.getValue(), txtDireccion.getValue(), txtRegion.getValue());
+            CargarTabla();
+            limpiarCrear();
+            btnCrear.setVisible(true);
+            btnInfo.setVisible(false);
+            btnCancel.setVisible(false);
+            capInfo.setLabel("Nuevo Contacto");
+            txtUser.setReadonly(false);
+        } else { // validar para cada campo obligatorio
+            alertaInfo("Faltan llenar algunos campos", "No se llenaron los campos obligatorios", null);
+        }
+    }
+
+    public void crear()
+    {
+        if (!txtPass.getValue().equals("") && !txtUser.getValue().equals("") && !txtNombre.getValue().equals("") &&
+                        !txtApellidos.getValue().equals("") && !txtDNI.getValue().equals("") &&
+                        cmbRole.getSelectedItem() != null && cmbEstado.getSelectedItem() != null) {
+            editar(null, txtUser.getValue(), txtPass.getValue(), txtNombre.getValue(), txtApellidos.getValue(),
+                            txtDNI.getValue(), txtTelefono.getValue(), txtMail.getValue(),
+                            (Boolean)cmbEstado.getSelectedItem().getValue(),
+                            ((DTO_Role) cmbRole.getSelectedItem().getAttribute("role")).getId(),
+                            txtCelular.getValue(), txtCiudad.getValue(), txtDireccion.getValue(), txtRegion.getValue());
+            CargarTabla();
+            limpiarCrear();
+        } else {
+            alertaInfo("Faltan llenar algunos campos", "No se llenaron algunos campos", null);
+        }
     }
 
     public void limpiarCrear()
@@ -301,6 +457,34 @@ public class PO_EAAdministrateEmployee
         } else {
             alertaError("Error en la carga de roles",
                             "error al cargar los roles", null);
+        }
+    }
+
+    public void alertaInfo(final String txt,
+                           final String txt2,
+                           final Throwable t)
+    {
+        if (txt.length() > 0) {
+            Messagebox.show(txt, company.getBusinessName(), 1, Messagebox.INFORMATION);
+        }
+        if (t != null) {
+            logger.info(txt2, t);
+        } else {
+            logger.info(txt2);
+        }
+    }
+
+    public void alertaError(final String txt,
+                            final String txt2,
+                            final Throwable t)
+    {
+        if (txt.length() > 0) {
+            Messagebox.show(txt, company.getBusinessName(), 1, Messagebox.EXCLAMATION);
+        }
+        if (t != null) {
+            logger.error(txt2, t);
+        } else {
+            logger.error(txt2);
         }
     }
 
